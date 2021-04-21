4ea8f00ba474ad408f4f8d35d073727b60f97bea
Baaji Shayeed Shaik <bshaik@teisd.com>
2021-04-21 00:04:53 -0500

    fixed styling and refactoring

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GameRoutingModule } from './game-routing.module';
-import { GameComponent } from './components/game.component';
+import { GameComponent } from './components/game/game.component';
 import { IonicModule } from '@ionic/angular';
+import { OptionsComponent } from './components/options/options.component';
 
 @NgModule({
-  declarations: [GameComponent],
+  declarations: [GameComponent, OptionsComponent],
   imports: [CommonModule, GameRoutingModule, IonicModule],
 })
 export class GameModule {}
